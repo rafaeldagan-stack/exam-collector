ebcb6fc5b1f284d2ff80d499eb02fbc91286b959
rafi <rafael.dagan@gmail.com>
2026-08-30 10:44:00 +0300

    Add scheduled exam collector runner

---
+++ run-collector.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /home/rafi/exam-collector || exit 1
+
+ENVIRONMENT="$1"
+
+if [ "$ENVIRONMENT" = "home" ]; then
+    INVENTORY="inventory-home.ini"
+elif [ "$ENVIRONMENT" = "lab" ]; then
+    INVENTORY="inventory.ini"
+else
+    echo "Usage: $0 {home|lab}"
+    exit 1
+fi
+
+ansible-playbook -i "$INVENTORY" collect-exams.yml
